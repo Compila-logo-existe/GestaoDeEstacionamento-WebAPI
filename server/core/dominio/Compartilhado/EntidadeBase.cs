9817091ba523aa5cb2e1b8b4818ebe25e5a6bc3a
gsvsantos <gustavocnsantos02@gmail.com>
2025-09-13 13:33:30 -0300

    refactor: Refatora geração dos Ids de entidades com Guid gerado diretamente na EntidadeBase.

--- server/core/dominio/Compartilhado/EntidadeBase.cs
+++ server/core/dominio/Compartilhado/EntidadeBase.cs
@@ -2,7 +2,7 @@ namespace GestaoDeEstacionamento.Core.Dominio.Compartilhado;
 
 public abstract class EntidadeBase<Tipo>
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public Guid UsuarioId { get; set; }
 
     public abstract void AtualizarRegistro(Tipo registroEditado);
